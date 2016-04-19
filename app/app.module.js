5db4c93a6922cd1aebae1b823fdd85cd6d2c83de
eliseubaldo <eliseu.baldo@gmail.com>
2016-04-18 20:32:12 -0400

    routing, materials, first views

--- app/app.module.js
+++ app/app.module.js
@@ -1,1 +1,19 @@
-var collectionApp = angular.module('collectionApp', []);
+var app = angular.module('collectionApp', ['ui.router', 'ngMaterial']);
+
+app.config(function ($stateProvider,$urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/home');
+
+	$stateProvider.state("home", {
+        url: "/home",
+        controller: "homeCtrl",
+        templateUrl: "app/views/home.html"
+    })
+
+    $stateProvider.state("newcollection", {
+        url: "/newcollection",
+        controller: "newCollectionCtrl",
+        templateUrl: "app/views/newcollection.html"
+    })
+  
+});
